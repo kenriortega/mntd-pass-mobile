bf7734cb83651fef8638d573d74a52fe7024dc44
kenriortega <kenriortega@gmail.com>
2020-06-08 16:00:16 -0400

    add: url base for get call to API

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,3 +2,7 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF6F35A5);
 const kPrimaryLightColor = Color(0xFFF1E6FF);
+
+// API_URL services
+
+const BASE_URL = 'http://192.168.43.119:8080/';
