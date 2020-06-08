a5df1e409526f7cac6a7f4a94360eb20dd5813b8
kenriortega <kenriortega@gmail.com>
2020-06-08 17:35:25 -0400

    refactor: Secret service for get data more clear

--- lib/services/secrets_service.dart
+++ lib/services/secrets_service.dart
@@ -1,20 +1,20 @@
 import 'dart:convert';
 
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'package:mntd_mobile/models/secret_model.dart';
 import 'package:mntd_mobile/utils/constants.dart';
 
 class SecretsService {
-  final String allSecretsUrl = "$BASE_URL/secrets";
+  final String allSecretsUrl = "$BASE_URL/secrets/kalix";
 
   Future<List<Secret>> getSecrets() async {
-    Response res = await get(allSecretsUrl);
+    var res = await http.get(allSecretsUrl);
 
     if (res.statusCode == 200) {
-      List<dynamic> body = jsonDecode(res.body);
-
+      var jsonResponse = jsonDecode(res.body);
+      List<dynamic> data = jsonResponse['data'];
       List<Secret> secrets =
-          body.map((dynamic item) => Secret.fromJson(item)).toList();
+          data.map((dynamic item) => Secret.fromJson(item)).toList();
       return secrets;
     } else {
       throw 'Can`t get secrets';
