57dc43a0cae7310821ec64d5a6c684bf7fb7a522
kenriortega <kenriortega@gmail.com>
2020-06-19 19:27:05 -0400

    add: field value for a model secret.

--- lib/models/secret_model.dart
+++ lib/models/secret_model.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 class Secret {
   final String username;
   final String name;
+  final String value;
   final String createdAt;
   final String category;
   final String img;
@@ -10,6 +11,7 @@ class Secret {
   Secret({
     @required this.username,
     @required this.name,
+    this.value,
     @required this.createdAt,
     @required this.category,
     @required this.img,
@@ -19,9 +21,19 @@ class Secret {
     return Secret(
       username: json['username'],
       name: json['name'],
+      value: json['value'],
       createdAt: json['createdAt'],
       category: json['category'],
       img: "assets/images/lock1.jpg",
     );
   }
+
+  Map toJson() => {
+        'username': username,
+        'name': name,
+        'value': value,
+        'category': category,
+        'createdAt': createdAt,
+        'img': img,
+      };
 }
