8e6a6725cb8b52580c55d9d4500f2cdca3795cbb
kenriortega <kenriortega@gmail.com>
2020-06-27 10:24:50 -0400

    refactor: remove unused packages

--- lib/services/secrets_service.dart
+++ lib/services/secrets_service.dart
@@ -2,7 +2,9 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:mntd_mobile/models/secret_model.dart';
+import 'package:mntd_mobile/providers/SecretCardProvider.dart';
 import 'package:mntd_mobile/utils/constants.dart';
+import 'package:provider/provider.dart';
 
 class SecretsService {
   final String allSecretsUrl = "$BASE_URL/secrets";
@@ -15,6 +17,7 @@ class SecretsService {
       List<dynamic> data = jsonResponse['data'];
       List<Secret> secrets =
           data.map((dynamic item) => Secret.fromJson(item)).toList();
+
       return secrets;
     } else {
       throw 'Can`t get secrets';
