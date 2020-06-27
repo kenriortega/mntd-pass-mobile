a9d5a0f8d5b089099b88c4c5bf5d1b4bb558d5bf
kenriortega <kenriortega@gmail.com>
2020-06-27 10:51:30 -0400

    fix: bug into home scroll layout

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -55,147 +55,152 @@ class _HomeState extends State<Home> {
     });
     return Scaffold(
       // backgroundColor: Color(0xFF2d3447),
-      body: Column(
-        children: <Widget>[
-          // menu & search actions
-          _isSearching
-              ? search(onSearchCancel, searching, _searchController)
-              : navBar(startSearching),
-          // SecretsPageScreen name text when will be show total of secrets by username
-          SizedBox(
-            height: 20,
-          ),
-          // Validate if secretCardProvider have data.
-          (Provider.of<SecretCardProvider>(context).getSecretsdLength() > 0
-              ? Consumer<SecretCardProvider>(
-                  builder: (context, secrets, child) => SingleChildScrollView(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            // menu & search actions
+            _isSearching
+                ? search(onSearchCancel, searching, _searchController)
+                : navBar(startSearching),
+            // SecretsPageScreen name text when will be show total of secrets by username
+            SizedBox(
+              height: 20,
+            ),
+            // Validate if secretCardProvider have data.
+            (Provider.of<SecretCardProvider>(context).getSecretsdLength() > 0
+                ? Consumer<SecretCardProvider>(
+                    builder: (context, secrets, child) => SingleChildScrollView(
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 20.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(
+                                  "Secrets #${secrets.getSecretsdLength()}",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 46,
+                                      fontFamily: "Calibre-Semibold",
+                                      letterSpacing: 1.0),
+                                ),
+                                Icon(Icons.storage)
+                                // IconButton(icon: Icon(CustomIcons.option,size: 12.0,color: Colors.white,), onPressed: (){})
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          // List of categories
+                          Categorylist(),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          // Stack of CardViews for a secrets by username
+                          Stack(
+                            children: <Widget>[
+                              CardScrollWidget(
+                                secrets: secrets.allSecrets,
+                                currentPage: currentPage,
+                              ),
+                              Positioned.fill(
+                                  child: PageView.builder(
+                                itemCount: secrets.getSecretsdLength(),
+                                controller: controller,
+                                reverse: true,
+                                itemBuilder: (context, index) {
+                                  return Container();
+                                },
+                              ))
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+
+                // if not have data make a sync request to API and render the same logic, Add snackbar that provide message to the user for this process
+                : SingleChildScrollView(
                     child: Column(
                       children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 20.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text(
-                                "Secrets #${secrets.getSecretsdLength()} locals",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 46,
-                                    fontFamily: "Calibre-Semibold",
-                                    letterSpacing: 1.0),
-                              ),
-                              // IconButton(icon: Icon(CustomIcons.option,size: 12.0,color: Colors.white,), onPressed: (){})
-                            ],
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          child: FutureBuilder(
+                            future: secretsService.getSecrets('kalix'),
+                            builder: (BuildContext context,
+                                AsyncSnapshot<List<Secret>> snapshot) {
+                              if (snapshot.hasData) {
+                                secrets = snapshot.data;
+                                Provider.of<SecretCardProvider>(context,
+                                        listen: false)
+                                    .addSecrets(secrets);
+                                return SingleChildScrollView(
+                                  child: Column(
+                                    children: <Widget>[
+                                      Padding(
+                                        padding: EdgeInsets.symmetric(
+                                            horizontal: 20.0),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: <Widget>[
+                                            Text(
+                                              "Secrets #${secrets.length}",
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 46,
+                                                  fontFamily:
+                                                      "Calibre-Semibold",
+                                                  letterSpacing: 1.0),
+                                            ),
+                                            Icon(Icons.sync)
+                                            // IconButton(icon: Icon(CustomIcons.option,size: 12.0,color: Colors.white,), onPressed: (){})
+                                          ],
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 20,
+                                      ),
+                                      // List of categories
+                                      Categorylist(),
+                                      SizedBox(
+                                        height: 20,
+                                      ),
+                                      // Stack of CardViews for a secrets by username
+                                      Stack(
+                                        children: <Widget>[
+                                          CardScrollWidget(
+                                            secrets: secrets,
+                                            currentPage: currentPage,
+                                          ),
+                                          Positioned.fill(
+                                              child: PageView.builder(
+                                            itemCount: secrets.length,
+                                            controller: controller,
+                                            reverse: true,
+                                            itemBuilder: (context, index) {
+                                              return Container();
+                                            },
+                                          ))
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                );
+                              }
+                              return Center(child: CircularProgressIndicator());
+                            },
                           ),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        // List of categories
-                        Categorylist(),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        // Stack of CardViews for a secrets by username
-                        Stack(
-                          children: <Widget>[
-                            CardScrollWidget(
-                              secrets: secrets.allSecrets,
-                              currentPage: currentPage,
-                            ),
-                            Positioned.fill(
-                                child: PageView.builder(
-                              itemCount: secrets.getSecretsdLength(),
-                              controller: controller,
-                              reverse: true,
-                              itemBuilder: (context, index) {
-                                return Container();
-                              },
-                            ))
-                          ],
                         )
                       ],
                     ),
-                  ),
-                )
-
-              // if not have data make a sync request to API and render the same logic, Add snackbar that provide message to the user for this process
-              : SingleChildScrollView(
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Container(
-                        child: FutureBuilder(
-                          future: secretsService.getSecrets('kalix'),
-                          builder: (BuildContext context,
-                              AsyncSnapshot<List<Secret>> snapshot) {
-                            if (snapshot.hasData) {
-                              secrets = snapshot.data;
-                              Provider.of<SecretCardProvider>(context,
-                                      listen: false)
-                                  .addSecrets(secrets);
-                              return SingleChildScrollView(
-                                child: Column(
-                                  children: <Widget>[
-                                    Padding(
-                                      padding: EdgeInsets.symmetric(
-                                          horizontal: 20.0),
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: <Widget>[
-                                          Text(
-                                            "Secrets #${secrets.length}",
-                                            style: TextStyle(
-                                                color: Colors.white,
-                                                fontSize: 46,
-                                                fontFamily: "Calibre-Semibold",
-                                                letterSpacing: 1.0),
-                                          ),
-                                          // IconButton(icon: Icon(CustomIcons.option,size: 12.0,color: Colors.white,), onPressed: (){})
-                                        ],
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 20,
-                                    ),
-                                    // List of categories
-                                    Categorylist(),
-                                    SizedBox(
-                                      height: 20,
-                                    ),
-                                    // Stack of CardViews for a secrets by username
-                                    Stack(
-                                      children: <Widget>[
-                                        CardScrollWidget(
-                                          secrets: secrets,
-                                          currentPage: currentPage,
-                                        ),
-                                        Positioned.fill(
-                                            child: PageView.builder(
-                                          itemCount: secrets.length,
-                                          controller: controller,
-                                          reverse: true,
-                                          itemBuilder: (context, index) {
-                                            return Container();
-                                          },
-                                        ))
-                                      ],
-                                    )
-                                  ],
-                                ),
-                              );
-                            }
-                            return Center(child: CircularProgressIndicator());
-                          },
-                        ),
-                      )
-                    ],
-                  ),
-                )),
-        ],
+                  )),
+          ],
+        ),
       ),
     );
   }
