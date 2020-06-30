6f1587d22c6ff4a892c32d08fe625648388843f3
ppmaluch <ppmaluch@gmail.com>
2020-06-30 15:46:27 -0400

    added: user data model and login service http requests

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,5 +4,5 @@ const kPrimaryColor = Color(0xFF6F35A5);
 const kPrimaryLightColor = Color(0xFFF1E6FF);
 
 // API_URL services
-const BASE_URL = 'http://169.254.236.166:8082';
-
+// const BASE_URL = 'http://169.254.236.166:8082'; //local URL
+const BASE_URL = 'https://kenri-mntd-pass-api.herokuapp.com'; //remote URL
