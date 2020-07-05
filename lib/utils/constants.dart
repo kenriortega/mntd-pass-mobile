ee62e8c1985b2d1fac33e0a763fe100428266c62
kenriortega <kenriortega@gmail.com>
2020-07-05 17:49:03 -0400

    [refactor] elements

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,17 @@
-const kDefaultPadding = 8.0;
+import 'package:flutter/material.dart';
+import 'package:mntd_mobile/models/secret_model.dart';
+import 'package:jiffy/jiffy.dart' as jiffy;
+
+import 'customIcons.dart';
+
+const kDefaultPadding = 20.0;
 const kCardAspectRatio = 12.0 / 16.0;
 const kWidgetAspectRatio = kCardAspectRatio * 1.2;
-
+const kDefaultShadow = BoxShadow(
+  offset: Offset(0, 4),
+  blurRadius: 4,
+  color: Colors.black26,
+);
 // API_URL services
 
 // const BASE_URL = 'http://169.254.236.166:8082'; //local URL
@@ -10,3 +20,13 @@ const BASE_URL = 'http://192.168.43.119:8080';
 // "mario", "msda930313@17"
 const USER_TEST = 'kalix';
 const PASS_TEST = 'kalix';
+
+String getTimeStamp(Secret secret) {
+  var jiffy1 = jiffy.Jiffy(secret.createdAt);
+  var jiffy2 = jiffy.Jiffy(DateTime.now().toIso8601String());
+  return secret.createdAt == null ? jiffy2.fromNow() : jiffy1.fromNow();
+}
+
+String getImagenByCategory(String category) {
+  return categoryItems[category];
+}
--- lib/utils/customIcons.dart
+++ lib/utils/customIcons.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 class CustomIcons {
-  static const IconData menu = IconData(0xe900,fontFamily: "CustomIcons");
-  static const IconData option = IconData(0xe902,fontFamily: "CustomIcons");
-
+  static const IconData menu = IconData(0xe900, fontFamily: "CustomIcons");
+  static const IconData option = IconData(0xe902, fontFamily: "CustomIcons");
 }
+
+Map<String, String> categoryItems = {
+  'personal': 'assets/images/personal.png',
+  'payments': 'assets/images/pyments.png',
+  'socials': 'assets/images/pyments.png',
+  'services': 'assets/images/personal.png',
+  'default': 'assets/images/lock1.jpg'
+};
